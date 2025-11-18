bee63189bab1a9a30d9c5595e4268aac3b313e3b
Khoi Anh Bui <anhkhoib207@gmail.com>
2025-11-18 22:20:05 +0700

    Create README

---
+++ src/components/AppSafeView.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppSafeView = () => {
+  return (
+    <View>
+      <Text>AppSafeView</Text>
+    </View>
+  )
+}
+
+export default AppSafeView
+
+const styles = StyleSheet.create({})
