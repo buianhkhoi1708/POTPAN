ae032cbec9376756c3901a46d34e91aabc8786cd
PKFire-223 <nhatquang2703@gmail.com>
2025-11-23 15:37:29 +0700

    fix

--- src/screens/page3-2a.tsx
+++ src/screens/page3-2a.tsx
@@ -1,3 +1,4 @@
+// src/screens/page3-2a.tsx
 import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
@@ -17,7 +18,7 @@ import styles from "../styles/screens/page3-2a.styles";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Page3-2a">;
 
-const COLOR = "#FF782C";
+const ORANGE = "#FF782C";
 const PEACH = "#FFE3D1";
 const PWD_PLACEHOLDER = "●".repeat(10);
 
@@ -184,94 +185,3 @@ const PasswordInput = ({
     </TouchableOpacity>
   </View>
 );
-<<<<<<< HEAD
-=======
-
-const styles = StyleSheet.create({
-  container: { flex: 1, paddingTop: 8, paddingHorizontal: 16 },
-  title: { 
-   
-    fontSize: 44, 
-    fontWeight: "700", 
-    textAlign: 'center', 
-    letterSpacing: 1,
-     marginBottom: 10, 
-     color: "#4B4B4B",
-     justifyContent: 'center',
-     fontStyle: 'normal',
-     marginTop: 8,
-    
-   },
-  form: { flex: 1,
-   },
-  label: { 
-  
-    fontSize: 24,
-    fontStyle: 'normal',
-    color: '#4B4B4B', 
-    fontWeight: '700',
-    marginBottom: 5, 
-    marginTop: 10 ,
-    },
-  inputWrap: { position: "relative" },
-  input: { 
-    height: 46, 
-    borderRadius: 40,
-     paddingHorizontal: 20, 
-     backgroundColor: COLOR, 
-     color: "#ffffffff", 
-     fontWeight: "600",
-     fontSize: 18,
-     letterSpacing:1,
-     textAlignVertical: "center"
-     },
-    textHolder:{
-    
-    fontSize: 18,
-    fontWeight: '500',
-    },
-  eyeBtn: { 
-    position: "absolute", 
-    right: 12, 
-    top: 11, 
-    height: 20, 
-    width: 20, 
-    alignItems: "center",
-     justifyContent: "center"
-     },
-  footer: { paddingTop: 8, paddingBottom: 28 },
-  termsText: { 
-
-    fontSize: 18, 
-    color: "#4B4B4B", 
-    lineHeight: 22, 
-    fontWeight:'500',
-    textAlign: 'center',
-  },
-  primaryBtn: { 
-    backgroundColor: COLOR, 
-    height: 48, 
-    width:185,
-    borderRadius: 40,
-     alignItems: "center", 
-     justifyContent: "center", 
-     marginTop: 10 ,
-     alignSelf: 'center',
-    },
-  primaryText: { 
-    color: "#fff", 
-    fontWeight: "800",
-    fontSize: 24,
-   
-
-     },
-  bottomText: { 
-    marginTop: 10, 
-    alignSelf: "center",
-     color: "#4B4B4B",
-    
-    fontSize: 18,
-     },
-  link: { color: '#1C37CF', fontWeight: "800" },
-});
->>>>>>> 2ab2f46a07a37a9ec351668391bd08bc9339c2a3
